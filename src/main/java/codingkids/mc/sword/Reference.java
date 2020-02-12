4a50f30e01d1884a3c137b97e6e04ebd6a592c24
Wilson Wu <iwilsonwu@gmail.com>
2020-02-12 13:44:35 +0800

    rename the modid

--- src/main/java/codingkids/mc/sword/Reference.java
+++ src/main/java/codingkids/mc/sword/Reference.java
@@ -5,7 +5,7 @@ package codingkids.mc.sword;
  */
 public class Reference
 {
-    public static final String MODID = "sword";
+    public static final String MODID = "cksword";
     public static final String NAME = "demo_sword";
     public static final String VERSION = "1.0";
     public static final String ACCEPTED_VERSION = "[1.12.2]";
